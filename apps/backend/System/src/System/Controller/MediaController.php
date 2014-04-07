3a987225012b8319f6d629d1db77cccc74243e0f
Manh Trung <mrtrungit@gmail.com>
2014-04-07 23:47:47 +0700

    add media

---
+++ apps/backend/System/src/System/Controller/MediaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace System\Controller;
+use MDS\Mvc\Controller\Action;
+class IndexController extends Action
+{
+    //  protected $aclPage = array('resource' => 'settings', 'permission' => 'user');
+    public function indexAction()
+    {
+        //  $arr = $this->getServiceLocator()->get('config');
+        //  echo "<pre>".print_r($arr,1)."</pre>";
+        return array();
+    }
+}
